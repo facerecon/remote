9ab8d3f9a2d24ef0c0a3f76b7d4ec071e7ecee65
Eric Sund <epsund@gmail.com>
2019-01-26 21:24:37 -0800

    got the angles just right

--- motors.py
+++ motors.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
 import time
-import curses
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -8,50 +7,45 @@ GPIO.setup(7,GPIO.OUT)
 GPIO.setup(11,GPIO.OUT)
 
 m = GPIO.PWM(11,50)
-m.start(6.5)
+m.start(7)
 
 p = GPIO.PWM(7,50)
-p.start(6.5)
+p.start(6)
 
 try:
 	while True:
 		key = raw_input("direction: ")
 		if key == 'q':
 			print("Going forward!")
-			m.ChangeDutyCycle(12)
+			m.ChangeDutyCycle(12.5)
 			#time.sleep(1)
 			print("Done!")
 		if key == "a":
 			print("Stationary!")
-			m.ChangeDutyCycle(6.5)
+			m.ChangeDutyCycle(7)
 			#time.sleep(1)
 			print("Done!")
 		if key == "z":
 			print("Going backward!")
-			m.ChangeDutyCycle(2.5)
+			m.ChangeDutyCycle(1.5)
 			#time.sleep(1)
 			print("Done!")
 
 		if key == "w":
 			print("Going left!")
-			p.ChangeDutyCycle(2.5)
+			p.ChangeDutyCycle(10)
 			#time.sleep(1)
 			print("Done!")
 		if key == "s":
 			print("Stationary!")
-			p.ChangeDutyCycle(6.5)
+			p.ChangeDutyCycle(6)
 			#time.sleep(1)
 			print("Done!")
 		if key == "x":
 			print("Stationary!")
-			p.ChangeDutyCycle(12)
+			p.ChangeDutyCycle(1.5)
 			#time.sleep(1)
 			print("Done!")
-			
-			
-			
-		
-		
 except KeyboardInterrupt:
 	p.stop()
 	m.stop()
