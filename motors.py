276e5d2ab6367fb9b0b6c3335f852c6dce62502c
Eric Sund <epsund@gmail.com>
2019-01-26 18:51:59 -0800

    now with direction wow

--- motors.py
+++ motors.py
@@ -1,28 +1,56 @@
 import RPi.GPIO as GPIO
 import time
+import curses
 
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(7,GPIO.OUT)
 GPIO.setup(11,GPIO.OUT)
 
-p = GPIO.PWM(7,50)
-p.start(7.5)
-
 m = GPIO.PWM(11,50)
-m.start(7.5)
+m.start(6.5)
+
+p = GPIO.PWM(7,50)
+p.start(6.5)
 
 try:
 	while True:
-		p.ChangeDutyCycle(7.5)
-		m.ChangeDutyCycle(7.5)
-		time.sleep(1)
-		p.ChangeDutyCycle(12.5)
-		m.ChangeDutyCycle(12.5)
-		time.sleep(1)
-		p.ChangeDutyCycle(2.5)
-		m.ChangeDutyCycle(2.5)
-		time.sleep(1)
+		key = raw_input("direction: ")
+		if key == 'q':
+			print("Going forward!")
+			m.ChangeDutyCycle(12)
+			#time.sleep(1)
+			print("Done!")
+		if key == "a":
+			print("Stationary!")
+			m.ChangeDutyCycle(6.5)
+			#time.sleep(1)
+			print("Done!")
+		if key == "z":
+			print("Going backward!")
+			m.ChangeDutyCycle(2.5)
+			#time.sleep(1)
+			print("Done!")
+
+		if key == "w":
+			print("Going left!")
+			p.ChangeDutyCycle(2.5)
+			#time.sleep(1)
+			print("Done!")
+		if key == "s":
+			print("Stationary!")
+			p.ChangeDutyCycle(6.5)
+			#time.sleep(1)
+			print("Done!")
+		if key == "x":
+			print("Stationary!")
+			p.ChangeDutyCycle(12)
+			#time.sleep(1)
+			print("Done!")
+			
+			
+			
+		
 		
 except KeyboardInterrupt:
 	p.stop()
